7569f1f890b869b70756ba67477e61837fa73cca
Trey Hunner <trey@treyhunner.com>
2015-07-14 16:44:32 -0700

    Use trailing commas

--- app/router.js
+++ app/router.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import config from './config/environment';
 
 let Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
 });
 
 Router.map(function() {
--- tests/helpers/resolver.js
+++ tests/helpers/resolver.js
@@ -5,7 +5,7 @@ let resolver = Resolver.create();
 
 resolver.namespace = {
   modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix
+  podModulePrefix: config.podModulePrefix,
 };
 
 export default resolver;
