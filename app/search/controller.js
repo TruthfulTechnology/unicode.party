b6740dde5771f803346f9b35edd999c99b92d788
Trey Hunner <trey@treyhunner.com>
2015-07-16 15:32:28 -0700

    Fix emoji searching & make emoji big

--- app/search/controller.js
+++ app/search/controller.js
@@ -1,19 +1,28 @@
 import Ember from 'ember';
 
-function keywordMatches (keyword, query) {
-  return keyword.indexOf(query.toLowerCase()) !== -1;
+function keywordMatches (keyword='', query='') {
+  let normalizedQuery = query.toLowerCase().replace(/\W*/, '');
+  return keyword.replace(/_/, '').indexOf(normalizedQuery) !== -1;
 }
 
 export default Ember.Controller.extend({
+  init() {
+    this._super();
+    this.updateEmojis();
+  },
   updateEmojis(query) {
+    this.set('emojiResults', []);
     var keywords = this.store.filter('keyword', keyword => {
       return keywordMatches(keyword.get('id'), query);
     }).then(keywordModels => {
       let keywordStrings = keywordModels.map(k => k.get('id'));
       return this.store.filter('emoji', emoji => {
-        return keywordStrings.some(keyword => {
+        let hasKeyword = keywordStrings.some(keyword => {
           return emoji.get('keywords').has(keyword);
         });
+        let nameMatches = keywordMatches(emoji.get('name'), query);
+        let categoryMatches = keywordMatches(emoji.get('category') || '', query);
+        return hasKeyword || nameMatches || categoryMatches;
       });
     }).then(matches => {
       this.set('emojiResults', matches);
