bae1353ba9ba967d97e39a4f28fa1cbeccbeb1df
Trey Hunner <trey@treyhunner.com>
2015-07-17 14:24:32 -0700

    Include Babel polyfill

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    babel: {
+      includePolyfill: true,
+    },
     fingerprint: {
       prepend: 'http://assets.unicode.party/',
     },
