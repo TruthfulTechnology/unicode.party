bcf835b81d313ec6f48885a538fad9e2fa030c6b
Trey Hunner <trey@treyhunner.com>
2015-07-17 16:00:53 -0700

    Track page views through Google Analytics

--- app/router.js
+++ app/router.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import config from './config/environment';
+import googlePageview from './mixins/google-pageview';
 
-let Router = Ember.Router.extend({
+let Router = Ember.Router.extend(googlePageview, {
   location: config.locationType,
 });
 
