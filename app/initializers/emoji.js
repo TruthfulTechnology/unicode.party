f224b084c12f08430b2f36c2c9fb2bffb0c9b76c
Trey Hunner <trey@treyhunner.com>
2015-07-16 23:31:28 -0700

    Do not include emoji without unicode characters

--- app/initializers/emoji.js
+++ app/initializers/emoji.js
@@ -4,14 +4,15 @@ export function initialize(container/* , application */) {
   let store = container.lookup('store:main');
   for (let name of Object.keys(emojis)) {
     let {char, category, keywords} = emojis[name];
+    if (!char) {
+      continue;
+    }
     if (keywords) {
       let keywordJSON = keywords.map(k => ({
         id: k,
         'type': 'keyword',
       }))
       store.push({data: keywordJSON});
-    }
-    if (!keywords) {
     }
     store.push(store.normalize('emoji', {id: name, name, char, category, keywords}));
   }
