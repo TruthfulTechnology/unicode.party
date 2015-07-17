547ccf56564f4baa2aa0c9183e58685548a1d14f
Trey Hunner <trey@treyhunner.com>
2015-07-17 10:27:09 -0700

    Add Google Analytics tracking code

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.googleAnalytics = {
+      webPropertyId: 'UA-60722966-2'
+    };
   }
 
   return ENV;
