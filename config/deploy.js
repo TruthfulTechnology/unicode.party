69d09c067a58a9338063850664d4edcd80131232
Trey Hunner <trey@treyhunner.com>
2016-04-11 22:03:13 -0700

    Fix deployment

--- config/deploy.js
+++ config/deploy.js
@@ -1,41 +1,25 @@
+/* eslint node: true */
 /* jshint node: true */
 
-module.exports = function(deployTarget) {
+module.exports = function (deployTarget) {
   var ENV = {
-    build: {}
-    // include other plugin configuration that applies to all deploy targets here
-  };
-
-  if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
-  }
-
-  if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
-  }
-
-  if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
-    ENV.s3 = {
+    build: {},
+    s3: {
       accessKeyId: process.env.AWS_KEY,
       secretAccessKey: process.env.AWS_SECRET,
       bucket: 'assets.unicode.party',
-    };
-    ENV['s3-index'] = {
+      region: 'us-east-1',
+    },
+    's3-index': {
       accessKeyId: process.env.AWS_KEY,
       secretAccessKey: process.env.AWS_SECRET,
       bucket: 'unicode.party',
-      acl: 'public-read',
+      region: 'us-east-1',
       hostName: 'unicode.party',
       indexMode: 'direct',
-    };
-  }
+      allowOverwrite: true,
+    },
+  };
 
-  // Note: if you need to build some configuration asynchronously, you can return
-  // a promise that resolves with the ENV object instead of returning the
-  // ENV object synchronously.
   return ENV;
 };
