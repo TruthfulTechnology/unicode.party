2f437954e81318131266fd462e51727b2bb21fd8
Trey Hunner <trey@treyhunner.com>
2022-06-03 23:10:05 -0700

    Start new site using vite and alpinejs

---
+++ new-site/main.js
@@ -0,0 +1,87 @@
+import './styles/app.scss';
+import emojidata from 'unicode-emoji-json';
+import emojilib from 'emojilib';
+import Alpine from 'alpinejs';
+
+
+function humanize(word, category) {
+  if (category === 'Flags' && word.match(/^flag_/)) {
+    return word.replace(/^flag_/, '').replace(/_/g, ' ');
+  }
+  return (word || '').replace(/_/g, ' ');
+}
+
+function scoreKeyword(keyword='', query='') {
+  let normalizedQuery = query.toLowerCase().replace(/\W*/, '');
+  if (keyword.indexOf(normalizedQuery) === -1) {
+    return 0;
+  }
+  return normalizedQuery.length / keyword.length;
+}
+
+
+class EmojiSearcher {
+
+  constructor() {
+    this.keywords = new Map();
+    this.emojis = new Map();
+    this.seedEmoji();
+  }
+
+  seedEmoji() {
+    for (let char of Object.keys(emojidata)) {
+      let {slug, group} = emojidata[char];
+      let keywords = emojilib[char];
+      let emoji = {name: humanize(slug, group), char};
+      if (!char) {
+        continue;
+      }
+      this.addKeyword(name, emoji);
+      this.addKeyword(humanize(group), emoji);
+      for (let k of keywords) {
+        this.addKeyword(k, emoji);
+      }
+    }
+  }
+
+  addKeyword(keyword, emoji) {
+    if (this.keywords.has(keyword)) {
+      this.keywords.get(keyword).add(emoji);
+    } else {
+      this.keywords.set(keyword, new Set([emoji]));
+    }
+  }
+
+  findEmoji(query) {
+    let matches = new Set();
+    let scores = new Map();
+    this.keywords.forEach((emojis, keyword) => {
+      let score = scoreKeyword(keyword, query);
+      if (score === 0) {
+        return;
+      }
+      for (let emoji of emojis) {
+        scores.set(emoji, (scores.get(emoji) || 0) + score);
+      }
+      matches = new Set([...matches, ...emojis]);
+    });
+
+    return Array.from(matches)
+        .map(m => ({name: m.name, char: m.char, score: scores.get(m) || 0}))
+        .sort((a, b) => b.score - a.score);  // reversed scoring
+  }
+
+}
+
+
+const searcher = new EmojiSearcher();
+
+Alpine.data('results', () => ({
+  search: '',
+  get filteredItems() {
+    return searcher.findEmoji(this.search);
+  },
+}));
+
+
+Alpine.start();
